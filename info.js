a64b30c7f9988b030c517a0b634043c9707e587d
Pum <michael.duengler@gmail.com>
2018-05-29 13:35:48 +0200

    Create info.js

---
+++ info.js
@@ -0,0 +1,7 @@
+{
+	"AUTHOR": "Pum",
+	"INSTALL_MSG": "Thank you for adding PumCogs to your Red bot instance.",
+	"NAME": "PumCogs",
+	"SHORT": "Cogs made by Pum",
+	"DESCRIPTION": "Cogs for Red Discord Bot made by Pum"
+}
