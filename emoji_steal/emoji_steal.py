635b7664dab2bcf02b786323696567e735b2ccca
Pum <michael.duengler@gmail.com>
2018-09-04 15:00:39 +0200

    Update emoji_steal.py

--- emoji_steal/emoji_steal.py
+++ emoji_steal/emoji_steal.py
@@ -3,6 +3,43 @@ import aiohttp
 from redbot.core import commands
 import discord
 
+
+class EmojiConverter(commands.Converter):
+    async def convert(self, ctx, emoji):
+        is_link_ = self.is_link(emoji)
+        if not is_link_:
+            base_link = "https://cdn.discordapp.com/emojis"
+            emoji_id = self.get_emoji_id(emoji)
+            if not emoji_id: return None
+            link = f"{base_link}/{emoji_id}.png"
+        else:
+            link = emoji
+        byte_emoji = await self.get_emoji(link)
+        return byte_emoji
+
+    @staticmethod
+    def get_emoji_id(emoji):
+        emoji_parts = emoji.split(":")
+        if emoji_parts[0] == "<a": return False
+        emoji_id = emoji_parts[1]
+        return emoji_id
+
+    @staticmethod
+    def is_link(emoji):
+        regex = re.compile(
+            r'^(?:http|ftp)s?://'  # http:// or https://
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+            r'(?::\d+)?'  # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+        return re.match(regex, emoji)
+
+    @staticmethod
+    async def get_emoji(link):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as result:
+                return await result.read()
+
 class EmojiSteal:
     def __init__(self, bot):
         self.bot = bot
@@ -56,39 +93,3 @@ class EmojiSteal:
         print("An error occurred:")
         traceback.print_exception(type(error), error, error.__traceback__)
         return False
-        
-class EmojiConverter(commands.Converter):
-    async def convert(self, ctx, emoji):
-        is_link_ = self.is_link(emoji)
-        if not is_link_:
-            base_link = "https://cdn.discordapp.com/emojis"
-            emoji_id = self.get_emoji_id(emoji)
-            if not emoji_id: return None
-            link = f"{base_link}/{emoji_id}.png"
-        else:
-            link = emoji
-        byte_emoji = await self.get_emoji(link)
-        return byte_emoji
-
-    @staticmethod
-    def get_emoji_id(emoji):
-        emoji_parts = emoji.split(":")
-        if emoji_parts[0] == "<a": return False
-        emoji_id = emoji_parts[1]
-        return emoji_id
-
-    @staticmethod
-    def is_link(emoji):
-        regex = re.compile(
-            r'^(?:http|ftp)s?://'  # http:// or https://
-            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-            r'(?::\d+)?'  # optional port
-            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-        return re.match(regex, emoji)
-
-    @staticmethod
-    async def get_emoji(link):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(link) as result:
-                return await result.read()
